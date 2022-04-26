3973ba77c9d1231e13658f0ba3536d8dbc55f7d5
Araks Babayan <Araks@Tigrans-MacBook-Pro.local>
2022-04-26 09:48:34 +0400

    Homework2

---
+++ 2.py
@@ -0,0 +1,19 @@
+'''2'''
+
+# mydict = {
+#     'Ani': 1 , 
+#     'Anna': 2 ,  
+#     'Zara': 3 , 
+#     'Lena': 4 ,  
+#     'Ira': 5 ,
+#     'Vardan': 6 ,
+#     'Yuri': 7 ,
+#     'Ara': 8 ,
+#     'Tigran': 9 , 
+#     'Sargis': 10
+#     }
+# # myddict = {}
+
+# res = sum(mydict.values()) / len(mydict)
+# print(f'the arithmetic average:' + str(res))
+
