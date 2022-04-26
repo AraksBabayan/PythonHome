404736dff804b7cc466157b3549103a3497ede9e
Araks Babayan <Araks@Tigrans-MacBook-Pro.local>
2022-04-26 09:51:31 +0400

    3Homework

---
+++ 3.py
@@ -0,0 +1,25 @@
+'''3   good,bed students'''
+
+
+# mydict = {
+#     'Ani': 1 , 
+#     'Anna': 2 ,  
+#     'Zara': 3 , 
+#     'Lena': 4 ,  
+#     'Ira': 5 ,
+#     'Vardan': 6 ,
+#     'Yuri': 7 ,
+#     'Ara': 8 ,
+#     'Tigran': 9 , 
+#     'Sargis': 10
+#     }
+# print(mydict)
+# good = []
+# bed = []
+
+# for i in mydict.values():
+#     if 5 >= i >= 1:
+#         bed.append(i)
+#     elif 6 <= i <= 10:
+#         good.append(i)
+# print(f'good---',good, '\nbed---', bed)
