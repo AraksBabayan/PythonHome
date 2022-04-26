5ef1a01702b16ebda8bae5dd77886feb159ff6a0
Araks Babayan <Araks@Tigrans-MacBook-Pro.local>
2022-04-26 09:56:34 +0400

    Homework5

---
+++ Homework5.py
@@ -0,0 +1,8 @@
+'''9 jnjel krknvox elementner@'''
+# old_list =[{'key1':'value1'}, {}, {}, {'key1':'value1'}, {'key2:value2'}]
+# print(old_list)
+# res = []
+# for i in old_list:
+#     if i not in res:
+#         res.append(i)
+# print(res)
