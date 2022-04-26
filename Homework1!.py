44204aea8bb89bbec4ded555cfdd656b885ac303
Araks Babayan <Araks@Tigrans-MacBook-Pro.local>
2022-04-26 09:40:57 +0400

    test

---
+++ Homework1!.py
@@ -0,0 +1,24 @@
+'''1'''
+# mydict = {}
+# mydict[1] = 'Ani'
+# mydict[2] = 'Anna'
+# mydict[3] = 'Zara'
+# mydict[4] = 'Lena'
+# mydict[5] = 'Ira'
+# mydict[6] = 'Vardan' 
+# mydict[7] = 'Yuri'
+# mydict[8] = 'Ara'
+# mydict[9] = 'Tigran' 
+# mydict[10] = 'Sargis'
+# print(mydict)
+
+
+
+'''kam'''
+# name = ['Ani', 'Anna', 'Zara', 'Lena', 'Ira', 'Vardan', 'Yuri', 'Ara', 'Tigran', 'Sargis']
+# age = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# mydict = {}
+# for i, j in zip(name, age):
+#     mydict.setdefault(i,j)
+# print(mydict)
+
