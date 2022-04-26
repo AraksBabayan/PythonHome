2b40aad1a8a659aaea53d72bf1474dfb84809b58
Araks Babayan <Araks@Tigrans-MacBook-Pro.local>
2022-04-26 09:54:52 +0400

    4Homework

---
+++ Homework4.py
@@ -0,0 +1,11 @@
+'''4-----------kisat'''
+# s = 'a 2 b 5 c 8 a 4 e 11'
+# mylist = []
+# mydict = {}
+# count = []
+# for i in s:
+#     mylist.append(s.count(i))
+
+# for x,y in zip(s, mylist):
+#     mydict.setdefault(x,y)
+# print(mydict)
