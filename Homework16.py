003262ccd26f6f94e7d720f5abe19ed4a1dcf713
Araks Babayan <Araks@Tigrans-MacBook-Pro.local>
2022-04-28 00:28:12 +0400

    Homework16

---
+++ Homework16.py
@@ -0,0 +1,8 @@
+'''8'''
+# patviratuhat = int(input('Enter value suvin or kaxazard:'))
+# gram = int(input('Enter gram:'))
+# sum = patviratuhat * gram 
+# if gram == 75 or gram == 112:
+#     print(sum, f'gram')
+# else:
+#     print('Enter 75 or 112')
