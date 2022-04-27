ea97ee0fac241825b3c87c96b0af9641c14942fe
Araks Babayan <Araks@Tigrans-MacBook-Pro.local>
2022-04-28 00:25:23 +0400

    Homework14

---
+++ Homework14.py
@@ -0,0 +1,6 @@
+'''6'''
+# vcharox = int(input('Enter sum:'))
+# quality = float(input('Enter quality:'))
+# quality1 = quality * 18 / 100 
+# print(quality1)
+# print(f'teasum:', vcharox - (quality + quality1))
