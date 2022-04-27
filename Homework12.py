a10a0533c0565bad7409bb98af64d91fbb36bf7e
Araks Babayan <Araks@Tigrans-MacBook-Pro.local>
2022-04-28 00:22:12 +0400

    Homework12

---
+++ Homework12.py
@@ -0,0 +1,8 @@
+'''5'''
+# botle = float(input('Enter bigbotle quality:')) 
+# xbig = 0.25
+# xsmall = 0.10
+# if botle <= 1:
+#     print(f'smallbotle ', xsmall, f'$')
+# if botle > 1:
+#     print(f'bigbottle', xbig, f'$')
