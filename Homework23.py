6aba6a02e56968baba4557a453e9ecd9c790b18f
Araks Babayan <Araks@Tigrans-MacBook-Pro.local>
2022-04-30 01:27:30 +0400

    Homework23

---
+++ Homework23.py
@@ -0,0 +1,15 @@
+'''Առանձնացնել տառերն ու թվերը'''
+
+
+'''text = 'python110'''
+
+
+# def myfunc(text, list1, list2):
+#     for i in text:
+#         if i.isdigit():
+#             list1.append(i)
+#         elif i.isalpha():
+#             list2.append(i)
+#     return list1, list2
+# print(myfunc('python110', [], []))
+
