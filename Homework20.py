caf99f4a32657a055d5e6cdf55d6a0fc7424674b
Araks Babayan <Araks@Tigrans-MacBook-Pro.local>
2022-04-30 01:12:18 +0400

    Homework20

---
+++ Homework20.py
@@ -0,0 +1,10 @@
+'''Max number two numbers'''
+
+# def maximum(a, b):
+#     if a >= b:
+#         return a
+#     else:
+#         return b
+# a = input('Enter num:')
+# b = input('Enter num:')
+# print(maximum(a, b))
