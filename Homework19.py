307d67ee03bf78116fd663479d017e42b65d7b0f
Araks Babayan <Araks@Tigrans-MacBook-Pro.local>
2022-04-30 01:10:31 +0400

    Homework19

---
+++ Homework19.py
@@ -0,0 +1,34 @@
+# '''calculation'''
+
+
+
+# def gum(x, y):
+#     return x + y
+# def bazm(x, y):
+#     return x * y
+# def baj(x, y):
+#     return x / y
+# def han(x, y):
+#     return x - y
+# print('Select operation:')
+# print('1.gum')
+# print('2. bazm')
+# print('3.baj')
+# print('4.han')
+# while True:
+#     choice = input('Enter choice(1/2/3/4):')
+#     if choice in('1', '2', '3', '4'):
+#         num1 = float(input('Enter first number:'))
+#         num2 = float(input('Enter second nimber:'))
+#         if choice == '1':
+#             print(num1, '+', num2, '=', gum(num1,num2))
+#         elif choice == '2':
+#             print(num1, '*', num2, '=', bazm(num1,num2))
+#         elif choice == '3':
+#             print(num1, '/', num2, '=', baj(num1,num2))
+#         elif choice == '4':
+#             print(num1, '-', num2, '=', han(num1,num2))
+#         d = input('Enter another:')
+#         if d == 'False':
+#             break
+#     else: print('Error')
