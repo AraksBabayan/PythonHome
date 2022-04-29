dfa4187cb184b1996e7be1639f4d7aa4b0d17231
Araks Babayan <Araks@Tigrans-MacBook-Pro.local>
2022-04-30 01:24:43 +0400

    Homework22

---
+++ Homework22.py
@@ -0,0 +1,12 @@
+'''multiply all numbers'''
+
+
+# def multiply(mylist):
+#     res = 1
+#     for i in mylist:
+#         res = res * i
+#         return res
+# list1 = [2, 3]
+# list2 = [2, 9]
+# print(multiply(list1))
+# print(multiply(list2))
