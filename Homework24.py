dfb671599edd66fd2dc65128e5347100a2673155
Araks Babayan <Araks@Tigrans-MacBook-Pro.local>
2022-05-03 09:26:23 +0400

    Homework

---
+++ Homework24.py
@@ -0,0 +1,13 @@
+'''85 Hipotenus by Pyutagoras '''
+
+# def hipos(x, y):
+#     h = (x ** 2 + y ** 2) ** 0.5
+#     return h
+# print(hipos(2,4))
+
+
+
+# # lenghtx = input('Enter a lenght:')
+# # lenghty = input('Enter b lenght:')
+# # print(hipos(lenghtx, lenghty))
+
