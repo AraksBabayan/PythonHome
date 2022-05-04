8f8ae19aaded43dbdf44db9ba1181dec5bee05f4
Araks Babayan <Araks@Tigrans-MacBook-Pro.local>
2022-05-05 01:18:54 +0400

    HW25

---
+++ HW25.py
@@ -0,0 +1,10 @@
+# def func(taxi):
+#     if 1 < taxi < 139:
+#         return f'4 $'
+#     if taxi == 140:
+#         return f'{4 + 0.25}'
+#     if taxi % 140 == 0:
+#         return (taxi / 140) * (4 + 0.25)
+    
+# taxi = float(input('Enter ml:'))
+# print(func(taxi))
