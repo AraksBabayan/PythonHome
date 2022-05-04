6f5957aa55b4012518a92783bce975870ee40154
Araks Babayan <Araks@Tigrans-MacBook-Pro.local>
2022-05-05 01:20:27 +0400

    HW26

---
+++ HW26.py
@@ -0,0 +1,19 @@
+'''code generator'''
+
+# import string
+# import random
+# alphabets = list(string.ascii_letters)
+# digits = list(string.digits)
+# special_charachters =list('!@~#$%^&*()')
+# characcters = list(string.ascii_letters +string.digits + '!@~#$%^&*()')
+# def generate_random_password():
+#     lenght = int(input('Enter lenght:'))
+#     random.shuffle(characcters)
+#     password = []
+#     for i in range(lenght):
+#         password.append(random.choice(characcters))
+#     random.shuffle(password)
+#     print("".join(password))
+# generate_random_password()
+
+
