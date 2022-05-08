81e6d8a441c41d86db2aff9b3b7596e3103ebde8
Araks Babayan <Araks@Tigrans-MacBook-Pro.local>
2022-05-08 18:12:30 +0400

    Json1

---
+++ json1.py
@@ -0,0 +1,15 @@
+# cars = {
+#     'BMW' : 1963,
+#     'Mercedes' : 1926,
+#     'Tesla' : 2003
+
+# }
+
+# with open('My.json', 'w') as file:
+#     json.dump(cars, file)
+
+# with open('My.json') as file:
+#     res = json.load(file)
+# print(res)
+# for i in res.items():
+#     print(i)
