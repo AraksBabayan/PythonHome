c3eb55ce0428530d7cc674027074a943fd946dea
Araks Babayan <Araks@Tigrans-MacBook-Pro.local>
2022-05-08 12:45:28 +0400

    Filehandle4

--- Filehandle3.py
+++ Filehandle3.py
@@ -3,3 +3,4 @@
 # with open('One.txt', 'a') as file:
 #     res = file.write('pen')
 #     print(res)
+
---
+++ Filehandle4.py
@@ -0,0 +1,12 @@
+'''4'''
+# with open('Teams.txt', 'w') as file:
+#     x = file.write('Hello hi barev aprogramming languages python js django ml')
+#     print(x)
+# with open('Teams.txt', 'rt') as file:
+#     res = file.read()
+#     print(res)
+#     res = res.split(' ')
+#     res.sort(key=len)
+#     print(res[-3:])
+# with open('Team.txt', 'a') as file:
+#     file.write(str(res))
