fd1b99efd1ba95bc5dc9bcd442a9b6c82f43ada5
Araks Babayan <Araks@Tigrans-MacBook-Pro.local>
2022-05-08 12:47:07 +0400

    Filehandle5

---
+++ Filehandle5.py
@@ -0,0 +1,15 @@
+'''5 tver@ gtnel stringic'''
+# with open('number.txt', 'w') as file:
+#     x = file.write('0, 1, 2, 3 ,4 , 5, 6, 7, 8, 9,a, h, k, l, o')
+#     print(x)
+# with open('number.txt', 'rt') as file:
+#     res = file.read()
+#     print(res)
+#     y_str = ' '
+#     for i in res:
+#         if i.isdigit():
+#             y_str = y_str + i
+#     print('Find nimbers:', y_str)
+
+
+
