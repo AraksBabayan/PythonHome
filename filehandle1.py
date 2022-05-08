df9ed7aeab1c71a3f682a121b3a6073e2a1804df
Araks Babayan <Araks@Tigrans-MacBook-Pro.local>
2022-05-08 12:37:20 +0400

    Filehandle1

---
+++ filehandle1.py
@@ -0,0 +1,15 @@
+'''print('------------------Homework---------------')'''
+# with open('One.txt', 'w') as file:
+#     file.write('Hayastan')
+
+# with open('Two.txt', 'w') as file:
+#     file.write('Sevan')
+
+# with open('Three.txt', 'w') as file:
+#     file.write('Leo')
+
+# with open('Four.txt', 'w') as file:
+#     file.write('Yerevan')
+
+# with open('Five.txt', 'w') as file:
+#     file.write('Erebuni \nKapan \nNew - York')
