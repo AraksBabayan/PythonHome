f7f19e99d41925e628597ce1c55fa725557989e2
Araks Babayan <Araks@Tigrans-MacBook-Pro.local>
2022-05-20 22:52:17 +0400

    Ampopich

--- json2.py
+++ json2.py
@@ -14,4 +14,42 @@
 #     print(res)
 
 
+# import simplejson as json
+# username = input('Enter name:')
+# with open('user.json', 'w') as file:
+#     json.dump(username, file)
+#     print(f'We ll remember when you come back, {username}!!') 
+
+
+
+# import simplejson as json
+
+# filename = 'user.json'
+# with open(filename) as file:
+#     user = json.load(file)
+#     print(f'Welcome back', {user})
+
+'''10.11,10.12'''
+
+# import simplejson as json
+# number = int(input('Enter your favorite number>>>'))
+# with open('favenum.json', 'w') as file:
+#     json.dump(number, file)
+
+# with open('favenum.json') as file:
+#     favenum = json.load(file)
+#     print(f'I know your favorite number!!!This is {favenum}')
+
+
+
+'''98'''
+
+'''parz tver'''
+# def func(x):
+#     x = int(input('Enter number:'))
+#     if x % x == 1 or x % 1 == x:
+#         return x
+# print(func(5))
+
+
 
