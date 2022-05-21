0976c0f73f3835b3fa19e92f54dfdf256870ac3f
Araks Babayan <Araks@Tigrans-MacBook-Pro.local>
2022-05-21 09:51:53 +0400

    test

--- a.py
+++ a.py
@@ -1,6 +1,6 @@
-# name = ['Ira,','Hasmik','Alla']
-# number = [20, 30, 40]
-# mydict = {key : Value
-#             for key, Value in zip(name,number)}
-# print(mydict)
-# import simplejson as json
+name = ['Ira,','Hasmik','Alla']
+number = [20, 30, 40]
+mydict = {key : Value
+            for key, Value in zip(name,number)}
+print(mydict)
+import simplejson as json
